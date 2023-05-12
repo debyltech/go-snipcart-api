d37a9cd1c3444df36c564fe3f8a253f4e22aca9d
Bastian de Byl <bastian@bdebyl.net>
2023-05-12 11:19:21 -0400

    main added CORSMiddleware

--- main.go
+++ main.go
@@ -12,6 +12,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func init() {
 	var err error
 	apiConfig, err = config.NewConfigFromEnv(true)
@@ -29,7 +45,8 @@ func init() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery(), CORSMiddleware())
 
 	api := r.Group("/api/snipcart")
 	{
